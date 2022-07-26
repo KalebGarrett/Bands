12efbb459603da16b26963423f68697081a99a57
Kaleb Garrett <kalebagarrett@gmail.com>
2022-07-26 19:23:43 -0400

    Added Info Page and updated layout.

---
+++ Bands.Models/Band.cs
@@ -0,0 +1,10 @@
+﻿namespace Bands.Models
+{
+    public class Band
+    {
+        public string Name { get; set; } 
+        public string Genre { get; set; }
+        public string Origin { get; set; }
+        
+    }
+}
---
+++ Bands.Web/Controllers/BandController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Bands.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bands.Web.Models;
+
+namespace Bands.Web.Controllers
+{
+    public class BandController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult BandInfo()
+        {
+            var model = new Band();
+            model.Name = "Yes";
+            model.Origin = "London, England";
+            model.Genre = "Rock";
+            return View(model);
+        }
+        
+        
+    }
+}
